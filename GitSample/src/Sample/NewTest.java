1cecf245bc7ba184f03bf1511453c241de337a4e
Selvakumar.Krishnamo <Selvakumar.Krishnamo@IN2074986W1.ey.net>
2016-06-25 20:44:05 +0530

    First Commit

---
+++ GitSample/src/Sample/NewTest.java
@@ -0,0 +1,20 @@
+package Sample;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class NewTest {
+
+  public void f(String test) {
+	  WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.lufthansa.com/ua/en/Homepage#");
+		driver.manage().window().maximize();
+		//@SuppressWarnings("unused")
+		String st=driver.getTitle();
+		String gt="Ready for";
+		Assert.assertEquals(st,gt);
+		System.out.println(test);
+		driver.quit();
+  }
+}
